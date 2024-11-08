e3376a6b4779dd9bc775177f969eb54f6abfb5d6
서동욱 <seonaru@yanolja.com>
2024-11-09 02:30:08 +0900

    Fix line endings settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'package:flutter/services.dart' show rootBundle;
-import 'dart:html' as html;
 
 import 'package:test_project/sampleCodeIm/sample_page_im.dart';
 
-
-
 void main() {
-
-  // main test
   runApp(MaterialApp(home: SamplePageIm()));
 }
